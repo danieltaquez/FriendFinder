d284cca1cd45e3f7090488ca92c3a04e63723d7d
Daniel Taquez <daniel.taquez@du.edu>
2019-11-23 16:35:11 -0700

    FD

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// Array holds data objects
+var friends = [
+    {
+        name: "Donald",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfXkpIbXWkPunfjMLO_xvbmw3eOvQ7_nuBzDHEL7A4IqKXudrZ",
+        answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
+    },
+    {
+        name: "SweetPea",
+        photo: "http://funhuh.com/wp-content/uploads/2014/06/01975_0271.jpg",
+        answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
+    },
+    {
+        name: "Cookie Monster",
+        photo: "https://www.askideas.com/media/10/Fat-Girl-Try-To-Eat-Funny-People.jpg",
+        answers: [5, 3, 5, 5, 5, 5, 1, 4, 4, 5]
+    },
+    {
+        name: "Unicorn",
+        photo: "http://www.funnyandhappy.com/wp-content/uploads/2014/f/funny-people-on-facebook-and-the-internet-4.jpg",
+        answers: [5, 1, 4, 1, 5, 1, 1, 4, 2, 5]
+    },
+];
+
+module.exports = friends;
